3768a73d76b3ab638f592efad4ee1afe6f22fa6c
jc <jc@pop-os.localdomain>
2022-08-30 23:28:51 +0200

    add fetch event details

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-
+import { gql } from "@apollo/client";
+import client from "../../apollo-client";
 import {
   EmojiHappyIcon,
   TicketIcon,
@@ -7,7 +8,8 @@ import {
   LinkIcon
 } from "@heroicons/react/outline";
 
-function Event() {
+function Event({ event }) {
+  console.log("EVENT:", event)
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Head>
@@ -23,12 +25,12 @@ function Event() {
         <div className="flex flex-wrap-reverse lg:flex-nowrap">
           <div className="w-full pr-0 lg:pr-24 xl:pr-32">
             <div className="mb-8 w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
-              
+
             </div>
             <p>description</p>
           </div>
           <div className="max-w-xs w-full flex flex-col gap-4 mb-6 lg:mb-0">
-           
+
             <div className="flex item-center">
               <UsersIcon className="w-6 mr-2" />
               <span className="truncate">
@@ -49,7 +51,7 @@ function Event() {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  
+
                 </a>
               </span>
             </div>
@@ -62,6 +64,42 @@ function Event() {
 
 export default Event;
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
+  const { id } = context.params;
 
+  const { data } = await client.query({
+    query: gql`
+      query Event($id: String!) {
+        event(id: $id) {
+          id
+          eventID
+          name
+          description
+          link
+          eventOwner
+          eventTimestamp
+          maxCapacity
+          deposit
+          totalRSVPs
+          totalConfirmedAttendees
+          imageURL
+          rsvps {
+            id
+            attendee {
+              id
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      id: id,
+    },
+  });
+
+  return {
+    props: {
+      event: data.event,
+    },
+  };
 }
