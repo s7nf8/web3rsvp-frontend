2c4947c7b2d3ad0cc906fb012d00bbe349b5156f
jc <jc@pop-os.localdomain>
2022-08-30 23:52:30 +0200

    finished section 7

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/[YOUR_GITHUB]/[YOUR_SUBGRAPH]",
+  uri: "https://api.thegraph.com/subgraphs/name/s7nf8/web3rsvp",
   cache: new InMemoryCache(),
 });
 
--- utils/connectContract.js
+++ utils/connectContract.js
@@ -3,24 +3,24 @@ import { ethers } from "ethers";
 
 function connectContract() {
     //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
-    const contractAddress = "0x[YOUR_CONTRACT_ADDRESS]";
+    const contractAddress = "0xd968B551e1E2F074B053E40e69620f83D757F5dE";
     const contractABI = abiJSON.abi;
     let rsvpContract;
     try {
-      const { ethereum } = window;
-  
-      if (ethereum) {
-        //checking for eth object in the window
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
+        const { ethereum } = window;
+
+        if (ethereum) {
+            //checking for eth object in the window
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+        } else {
+            console.log("Ethereum object doesn't exist!");
+        }
     } catch (error) {
-      console.log("ERROR:", error);
+        console.log("ERROR:", error);
     }
     return rsvpContract;
-  }
-  
-  export default connectContract;
+}
+
+export default connectContract;
